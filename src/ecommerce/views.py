5c224b5c03be722974e19ce8d68ad5b5c18734ea
Huy Nguyen <huynguyen260398@gmail.com>
2019-06-17 11:44:50 +0700

    Added Register page.

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,15 +1,17 @@
 # from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render, redirect
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
     context = {
         'title': 'Home Page',
-        'content': 'Welcome to the home page!'
+        'content': 'Welcome to the home page!',
     }
+    if request.user.is_authenticated():
+        context['premium_content'] = 'Premium Content!!!'
     return render(request, 'home_page.html', context)
 
 
@@ -59,7 +61,7 @@ def login_page(request):
             login(request, user)
             # Redirect to a success page.
             # context['form'] = LoginForm()
-            return redirect('/login')
+            return redirect('/')
         else:
             # Return an 'invalid login' error message.
             print('Error')
@@ -67,5 +69,21 @@ def login_page(request):
     return render(request, 'auth/login.html', context)
 
 
+User = get_user_model()
+
+
 def register_page(request):
-    return render(request, 'auth/register.html', {})
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+
+    return render(request, 'auth/register.html', context)
