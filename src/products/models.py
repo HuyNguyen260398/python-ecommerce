91fcaccce6668f2071db80be15ba454ce2ed059d
Huy Nguyen <huynguyen260398@gmail.com>
2019-06-23 17:24:00 +0700

    Added image field.

--- src/products/models.py
+++ src/products/models.py
@@ -1,4 +1,25 @@
+import random
+import os
 from django.db import models
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 1000000)
+    name, ext = get_filename_ext(filename)
+    final_filname = '{new_filename}{ext}'.format(
+        new_filename=new_filename,
+        ext=ext
+    )
+    return "products/{new_filename}/{final_filname}".format(
+        new_filename=new_filename,
+        final_filname=final_filname
+    )
 
 
 class Product(models.Model):
@@ -8,6 +29,11 @@ class Product(models.Model):
         decimal_places=2,
         max_digits=20,
         default=40.00
+    )
+    image = models.ImageField(
+        upload_to=upload_image_path,
+        null=True,
+        blank=True
     )
 
     def __str__(self):
