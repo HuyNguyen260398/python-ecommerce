c9fce1e78c424a1093119308b978aabefaca17e1
Huy Nguyen <huynguyen260398@gmail.com>
2019-06-26 08:01:24 +0700

    Added Bootstrap example page.

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 # from products.views import (
 #     ProductListView,
@@ -40,6 +41,10 @@ urlpatterns = [
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
     url(r'^products/', include('products.urls', namespace='products')),
+    url(
+        r'^bootstrap/$',
+        TemplateView.as_view(template_name='bootstrap/example.html')
+    ),
     # url(r'^featured/$', ProductFeatureListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeatureDetailView.as_view()),
     # url(r'^products/$', ProductListView.as_view()),
