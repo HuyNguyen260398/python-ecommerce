b7063f180a0e43ce42f61baac9f1d4248927dfd3
Huy Nguyen <huynguyen260398@gmail.com>
2019-07-03 09:45:26 +0700

    Added Cart model.

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get('cart_id', None)
+    if cart_id is None:
+        print('create new cart')
+        request.session['cart_id'] = 1
+    else:
+        print('cart id exists')
     return render(request, 'carts/home.html', {})
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -39,13 +39,6 @@ urlpatterns = [
         r'^bootstrap/$',
         TemplateView.as_view(template_name='bootstrap/example.html')
     ),
-    # url(r'^featured/$', ProductFeatureListView.as_view()),
-    # url(r'^featured/(?P<pk>\d+)/$', ProductFeatureDetailView.as_view()),
-    # url(r'^products/$', ProductListView.as_view()),
-    # url(r'^products-fbv/$', product_list_view),
-    # # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-    # url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
-    # url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
 
