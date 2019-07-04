6a233d382080d03b34f0e8595a474e25bf4ed4e0
Huy Nguyen <huynguyen260398@gmail.com>
2019-07-04 09:23:16 +0700

    Added calculate total and subtotal functions.

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,4 +5,10 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, 'carts/home.html', {})
