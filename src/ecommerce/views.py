24dd2de0d1669b17fa33fb8a4c4860c24e74977e
Huy Nguyen <huynguyen260398@gmail.com>
2019-08-06 16:47:31 +0700

    Ajaxify Contact Form.

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,5 @@
-# from django.http import HttpResponse
-# from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render
 
 from .forms import ContactForm
@@ -31,7 +31,13 @@ def contact_page(request):
         'form': contact_form
     }
     if contact_form.is_valid():
-        print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({'message': 'Thank you for your submission!'})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
 
     # if request.method == 'POST':
     #     # print(request.POST)
