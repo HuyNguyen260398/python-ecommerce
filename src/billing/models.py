c87c249f05880eaa07e3a45bc6bf58835ffc3be5
Huy Nguyen <huynguyen260398@gmail.com>
2019-08-09 08:55:38 +0700

    Installed Stripe and created Stripe customer.

--- src/billing/models.py
+++ src/billing/models.py
@@ -1,11 +1,15 @@
+import stripe
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from accounts.models import GuestEmail
 
 
 User = settings.AUTH_USER_MODEL
+
+
+stripe.api_key = "sk_test_L2UkxaY9kJLqL3veVS0fCuLv00uVo6w8I4"
 
 
 class BillingProfileManager(models.Manager):
@@ -32,11 +36,24 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
 
     objects = BillingProfileManager()
 
     def __str__(self):
         return self.email
+
+
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        customer = stripe.Customer.create(
+            email=instance.email
+        )
+        instance.customer_id = customer.id
+        print(customer)
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 
 def user_created_reciever(sender, instance, created, *args, **kwargs):
