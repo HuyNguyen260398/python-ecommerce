19048faf01081c82e061c15521a3b276e9ead6c5
Huy Nguyen <huynguyen260398@gmail.com>
2019-08-15 10:12:30 +0700

    Added user account home view.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,11 +1,26 @@
 from django.contrib.auth import authenticate, login, get_user_model
-from django.views.generic import CreateView, FormView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, FormView, View, DetailView
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
 from .models import GuestEmail
 from .forms import LoginForm, RegisterForm, GuestForm
 from .signals import user_logged_in_signal
+
+
+# @login_required
+# def account_home_view(request):
+#     return render(request, "accounts/home.html", {})
+
+
+class AccountHomeView(LoginRequiredMixin, DetailView):
+    template_name = 'accounts/home.html'
+
+    def get_object(self):
+        return self.request.user
 
 
 def guest_register_view(request):
