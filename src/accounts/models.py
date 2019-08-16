342a47c6ef41e28dc62389a23449850b4615865d
Huy Nguyen <huynguyen260398@gmail.com>
2019-08-16 22:11:29 +0700

    Added Email Activation View.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.contrib.auth.models import (
@@ -144,7 +145,7 @@ class EmailActivation(models.Model):
             user = self.user
             user.is_active = True
             user.save()
-            self.activate = True
+            self.activated = True
             self.save()
             return True
         return False
@@ -160,7 +161,7 @@ class EmailActivation(models.Model):
         if not self.activated and not self.forced_expired:
             if self.key:
                 base_url = getattr(settings, 'BASE_URL', 'https://www.prj-ecom-pydj.herokuapp.com/')
-                key_path = self.key
+                key_path = reverse("account:email-activate", kwargs={'key': self.key})
                 path = "{base}{path}".format(base=base_url, path=key_path)
                 context = {
                     'path': path,
