c519fa7cb4ed387fa9cb446337ed24df13c688c1
Huy Nguyen <huynguyen260398@gmail.com>
2019-08-18 19:03:02 +0700

    Added List and Detail view for Orders.

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -48,6 +48,7 @@ urlpatterns = [
     url(r'^billing/payment-method/create/$', payment_method_create_view,
         name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
